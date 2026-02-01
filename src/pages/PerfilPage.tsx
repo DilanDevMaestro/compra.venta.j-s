6a74fdb69eff003658f02abdbe6a0ce46607bfca
DilanDevMaestro <maestrodilandev@gmail.com>
2026-02-01 16:20:51 -0300

    agf

--- src/pages/PerfilPage.tsx
+++ src/pages/PerfilPage.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
+import type { FormEvent } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Header } from '../components/layout/Header'
 import { Footer } from '../components/layout/Footer'
